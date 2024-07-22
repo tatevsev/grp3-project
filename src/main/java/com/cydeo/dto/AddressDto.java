b56c52ff86653e0b6a2d687be58efc610b7ac3ee
fatihduzenli <fatihdznl1991@gmail.com>
2024-07-22 17:00:26 -0400

    AddressDto is created

--- src/main/java/com/cydeo/dto/AddressDto.java
+++ src/main/java/com/cydeo/dto/AddressDto.java
@@ -1,4 +1,17 @@
 package com.cydeo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class AddressDto {
+   private String addressLine1;
+   private String addressLine2;
+   private String city;
+   private String state;
+   private String country;
+   private String zipCode;
 }
