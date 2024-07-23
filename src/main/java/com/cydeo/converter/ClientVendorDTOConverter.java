e75602337f831c69e82c424913b57452817bfea7
fatihduzenli <fatihdznl1991@gmail.com>
2024-07-22 20:03:44 -0400

    ClientVendorDto converter created

---
+++ src/main/java/com/cydeo/converter/ClientVendorDTOConverter.java
@@ -0,0 +1,22 @@
+package com.cydeo.converter;
+
+import com.cydeo.dto.ClientVendorDto;
+import com.cydeo.service.ClientVendorService;
+import org.springframework.core.convert.converter.Converter;
+
+public class ClientVendorDTOConverter implements Converter<String, ClientVendorDto> {
+
+    private final ClientVendorService clientVendorService;
+
+    public ClientVendorDTOConverter(ClientVendorService clientVendorService) {
+        this.clientVendorService = clientVendorService;
+    }
+
+    @Override
+    public ClientVendorDto convert(String source) {
+        if (source == null || source.equals("")) {
+            return null;
+        }
+        return clientVendorService.findByClientVendorName(source);
+    }
+}
