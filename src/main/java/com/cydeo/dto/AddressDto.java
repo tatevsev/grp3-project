2b969d4b548d369acb72c3cf54651275981b1466
gozdegunduz <gunduzgzd@gmail.com>
2024-07-23 11:21:31 -0400

    everything has been merged

--- src/main/java/com/cydeo/dto/AddressDto.java
+++ src/main/java/com/cydeo/dto/AddressDto.java
@@ -12,4 +12,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @Setter
 public class AddressDto {
+
+    private Long id;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
 }
