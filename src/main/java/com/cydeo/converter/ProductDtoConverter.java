91fb957c20ce85b2be73f14fee252131048d6546
tatevsev <149326385+tatevsev@users.noreply.github.com>
2024-07-24 00:59:37 +0200

    Created ProductDtoConverter

---
+++ src/main/java/com/cydeo/converter/ProductDtoConverter.java
@@ -0,0 +1,29 @@
+package com.cydeo.converter;
+
+import com.cydeo.dto.ProductDto;
+import com.cydeo.service.ProductService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+
+public class ProductDtoConverter implements Converter<String, ProductDto> {
+
+    private final ProductService productService;
+
+    public ProductDtoConverter( @Lazy ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Override
+    public ProductDto convert(String source) {
+        if (source == null || source.equals("")) {
+            return null;
+        }
+
+        return productService.getProductById(Long.valueOf(source));
+    }
+}
