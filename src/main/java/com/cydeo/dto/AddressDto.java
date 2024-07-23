85e49e6d44edc4e4b2975762ae3c2df1999afb06
fatihduzenli <fatihdznl1991@gmail.com>
2024-07-22 22:59:55 -0400

    Error fixed

--- src/main/java/com/cydeo/dto/AddressDto.java
+++ src/main/java/com/cydeo/dto/AddressDto.java
@@ -1,12 +1,11 @@
 package com.cydeo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 public class AddressDto {
 
    private Long id;
