feaaacfabb578ba8decd2f37c2e775f7423b428f
sattar <m.sallihemin14@gmail.com>
2024-07-23 12:03:04 -0400

    Create product entity & productDTO & enum JD-US2-GR3

--- src/main/java/com/cydeo/entity/Product.java
+++ src/main/java/com/cydeo/entity/Product.java
@@ -1,9 +1,34 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import com.cydeo.enums.ProductUnit;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
+@Data
+@Table(name = "products")
 public class Product extends BaseEntity {
+
+   private String name;
+
+   private int quantityInStock;
+
+   private int lowLimitAlert;
+
+   @Enumerated(EnumType.STRING)
+   private ProductUnit productUnit;
+
+   @ManyToOne
+   private Category category;
+
+
+
+
+
+
+
 }
