6a4b7bed0349776a2b68ba4a9540de2e31a62a07
tatevsev <149326385+tatevsev@users.noreply.github.com>
2024-07-24 13:09:00 +0200

    Product Controller class is created

---
+++ src/main/java/com/cydeo/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.cydeo.controller;
+
+import com.cydeo.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/list")
+    public String listAllProducts(Model model) {
+        model.addAttribute("products", productService.listAllProducts());
+        return "/product/product-list";
+    }
+
+
+
+}
