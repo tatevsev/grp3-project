4d3acd168f8e12bf197c343ec420af8ed6543420
tatevsev <149326385+tatevsev@users.noreply.github.com>
2024-07-24 02:00:45 +0200

    Made some changes

--- src/main/java/com/cydeo/entity/InvoiceProduct.java
+++ src/main/java/com/cydeo/entity/InvoiceProduct.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.math.BigDecimal;
@@ -24,8 +25,10 @@ public class InvoiceProduct extends BaseEntity {
     private BigDecimal profitLoss;
     private int remainingQuantity;
     @ManyToOne
+    @JoinColumn(name = "invoice_id")
     private Invoice invoice;
     @ManyToOne
+    @JoinColumn(name = "product_id" )
     private Product product;
 
 
